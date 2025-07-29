31c8cfc70b35492ddd5ffe88ba0f607d46f7b205
dieegoZX <115849164+dieegoZX@users.noreply.github.com>
2025-07-29 16:07:12 -0400

    Create jeepModels.js

---
+++ data/jeepModels.js
@@ -0,0 +1,31 @@
+const jeepModels = [
+  {
+    nome: 'Renegade',
+    motor: '1.3 Turbo Flex',
+    potencia: '185 cv',
+    torque: '27,5 kgfm',
+    cambio: 'Automático de 6 marchas',
+    tracao: '4x2 ou 4x4',
+    consumo: '10,1 km/l (misto)',
+  },
+  {
+    nome: 'Compass',
+    motor: '2.0 Turbo Diesel',
+    potencia: '170 cv',
+    torque: '35,7 kgfm',
+    cambio: 'Automático de 9 marchas',
+    tracao: '4x4',
+    consumo: '11,4 km/l (misto)',
+  },
+  {
+    nome: 'Commander',
+    motor: '2.0 Turbo Diesel',
+    potencia: '170 cv',
+    torque: '38,7 kgfm',
+    cambio: 'Automático de 9 marchas',
+    tracao: '4x4',
+    consumo: '10,5 km/l (misto)',
+  }
+]
+
+module.exports = jeepModels
