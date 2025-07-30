d5bd5bdb0b880ce76961d00010505f75d94b7cd6
dieegoZX <115849164+dieegoZX@users.noreply.github.com>
2025-07-30 08:19:49 -0400

    Delete origin directory

--- origin/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const express = require('express')
-const routes = require('./protocol/routes')
-
-const app = express()
-app.use(express.json())
-app.use(routes)
-
-app.listen(8080, () => console.log('Servidor MCP no ar 🚀'))
